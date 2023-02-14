ceb3e33e24e59aba1e641885788bd987ed42c63e
Jasurbek Shomaqsudov <107366287+Jasurbek2208@users.noreply.github.com>
2023-02-14 15:41:04 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,116 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 
-let portfolios = [
-    {
-        title: "Portfolio 1",
-        image: "https://via.placeholder.com/150",
-        "project-link": "https://example.com",
-        "github-link": "https://github.com/example"
-    },
-    {
-        title: "Portfolio 2",
-        image: "https://via.placeholder.com/150",
-        "project-link": "https://example.com",
-        "github-link": "https://github.com/example"
-    }
-];
+// Portfolios
+// const portfolios = JSON.parse(process.env.USERS_DB);
 
-app.get('/portfolios', (req, res) => {
-    res.json(portfolios);
+// users
+// let users = JSON.parse(process.env.USERS_DB);
+
+// Register
+app.post("/auth/register", (req, res) => {
+  const obj = req.body;
+  const name = JSON.parse(obj.body).name;
+  const password = JSON.parse(obj.body).password;
+
+  if (typeof name !== "string" || typeof password !== "string") {
+    res.status(300);
+    res.json({ message: "The type of name and password must be String!" });
+    
+  } else if (users.find((user) => user.name === name)) {
+    res.status(400);
+    res.json({ message: "This username is already registered." });
+    
+  } else if (String(name).length > 4 &&
+    String(name).length < 16 &&
+    String(password).length > 4 &&
+    String(password).length < 12
+  ) {
+    const user = {
+      name: name,
+      password: password,
+      _id: String(new Date().getTime()),
+      access_token:
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15),
+    };
+
+    users.push(user);
+    process.env.USERS_DB = JSON.stringify(users);
+    
+    res.json({
+      message: "You have successfully registered.",
+      user: { _id: user._id, name: user.name },
+      token: user.access_token,
+    });
+    
+  } else {
+    res.status(401);
+    res.json({
+      message: `invalid require. 'name' must be at least 4 and at most 16 characters. The 'password' must be at least 4 and at most 12 characters long.`,
+    });
+  }
 });
 
-app.post('/portfolios', (req, res) => {
-    const newPortfolio = req.body;
-    portfolios.push(newPortfolio);
-    res.json(portfolios);
+// Login
+app.post("/auth/login", (req, res) => {
+  const obj = req.body;
+  const name = JSON.parse(obj.body).name;
+  const password = JSON.parse(obj.body).password;
+
+  if (users.find((user) => user.name === name && user.password === password)) {
+    let currentUser = users.find((user) => (user.name === name ? user : null));
+
+    res.json({
+      message: "success",
+      user: { _id: currentUser._id, name: currentUser.name },
+      token: currentUser.access_token,
+    });
+  } else {
+    res.status(401);
+    res.json({ message: "invalid login or password." });
+  }
 });
 
-app.listen(2208, () => {
-    console.log('Server started on port 2208');
+// UserME
+app.get("/auth/userme", (req, res) => {
+  const token = req.headers.authorization;
+  
+  if (!token) {
+    res.status(400);
+    return res.json({ message: "Token not found in request headers." });
+  }
+
+  const currentUser = users.find(user => user.access_token === token);
+  if (!currentUser) {
+    res.status(401);
+    return res.json({ message: "Unauthorized. Token not found in database." });
+  }
+
+  res.status(200);
+  return res.json({ user: { _id: currentUser._id, name: currentUser.name } });
+});
+
+
+// GET Portfolios
+app.get("/portfolios", (req, res) => {
+  res.json(JSON.parse(process.env.USERS_DB));
+});
+
+// POST Portfolios
+app.post("/portfolios", (req, res) => {
+  const newPortfolio = req.body;
+  portfolios.push(newPortfolio);
+  res.json(portfolios);
+});
+
+//
+
+// Run the server and report out to the logs
+app.listen(4004, () => {
+  console.log("Server started on port 4004");
 });
