a22db01810eda5cee6067ad953e13429dfbf5c00
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-07-25 17:15:10 +0300

    test bag

--- index.js
+++ index.js
@@ -14,21 +14,6 @@ app.use(cors());
 // Secret Key for jwt
 const secretkey = config.get('SECRET_KEY')
 
-// Portfolios
-let portfolios = JSON.parse(JSON.stringify(config.get('PORTFOLIOS')));
-
-// users
-const users = JSON.parse(JSON.stringify(config.get('USERS')));
-
-// Access Token generator
-function generateAccessToken(user) {
-  const payload = { ...user }
-
-  const options = { expiresIn: '1h' }
-
-  return jwt.sign(payload, secretkey, options)
-}
-
 
 // Run the server and report out to the logs
 app.listen(port, () => {
